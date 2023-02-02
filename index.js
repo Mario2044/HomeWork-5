a5c502d78d65d50d0f34eca21e9cc15fd84ad206
Mario2044 <116796406+Mario2044@users.noreply.github.com>
2023-02-02 21:02:14 +0200

    fix

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 let arr = prompt("Choice what you need (+)(-)(/)(*)?");
-let numOne = prompt("Tell me number.");
-let numTwo = prompt("Tell me number.");
-let sum = +numOne + +numTwo;
-let summ = -numOne - -numTwo;
-let suma = numOne / numTwo;
-let summa = numOne * numTwo;
+let twoNum = prompt("Write two numbers separated by commas.");
+let twoNumbers = twoNum.split(',');
+let numOne = twoNumbers[0];
+let numTwo = twoNumbers[1];
+let sumAdd = +twoNumbers[0] + +twoNumbers[1];
+let sumSub = +twoNumbers[0] - +twoNumbers[1];
+let sumMult = +twoNumbers[0] * +twoNumbers[1];
+let sumDiv = +twoNumbers[0] / +twoNumbers[1];
 
 while(arr !== "+" && arr !== "-" && arr !== "/" && arr !== "*" && arr !== " ") {
     console.log("Sorry, you did not enter any of the options", arr);
     arr = prompt("Choice what you need (+)(-)(/)(*)?");
-    numOne = prompt("Tell me number.");
-    numTwo = prompt("Tell me number.");
-}
+} 
 
 if(arr === "+") {
-    console.log(`${numOne} + ${numTwo} = ${sum}`);
+    console.log(`${twoNumbers[0]} + ${twoNumbers[1]} = ${sumAdd}`);
 }
 else  if(arr === "-") {
-    console.log(`${numOne} - ${numTwo} = ${summ}`);
-}
-else  if(arr === "/") {
-    console.log(`${numOne} / ${numTwo} = ${suma}`);
+    console.log(`${twoNumbers[0]} - ${twoNumbers[1]} = ${sumSub}`);
 }
 else  if(arr === "*") {
-    console.log(`${numOne} * ${numTwo} = ${summa}`);
+    console.log(`${twoNumbers[0]} * ${twoNumbers[1]} = ${sumMult}`);
 }
+else  if(arr === "/") {
+    console.log(`${twoNumbers[0]} / ${twoNumbers[1]} = ${sumDiv}`);
+}
+
 
